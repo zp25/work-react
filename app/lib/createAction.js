7c81db4fb616725b03148a9567c8b3181e83da22
zp25 <zebrap25@gmail.com>
2017-05-22 16:44:04 +0800

    style: 整理createAction, createReducer，更新注释

--- app/lib/createAction.js
+++ app/lib/createAction.js
@@ -2,40 +2,42 @@ import 'babel-polyfill';
 
 /**
  * FSA Factory
- * https://github.com/acdlite/redux-actions/blob/master/src/createAction.js
- * @param {String} type Action type
- * @param {Function} actionCreator payload创建函数
- * @param {Function} metaCreator meta数据创建函数
- * @return {Function} Action创建函数，此函数返回创建的Action
+ * @see {@link https://github.com/acdlite/redux-actions/blob/master/src/createAction.js}
+ * @param {string} type - Action type
+ * @param {function} [payloadCreator] - payload创建函数
+ * @param {function} [metaCreator] - meta数据创建函数
+ * @return {function} Action创建函数
  */
-export default (type, actionCreator, metaCreator) => {
-  const finalActionCreator = typeof actionCreator === 'function'
-    ? actionCreator
+export default (type, payloadCreator, metaCreator) => {
+  const finalPayloadCreator = typeof payloadCreator === 'function'
+    ? payloadCreator
     : t => t;
 
-  const actionHandler = (...args) => {
+  const hasMeta = typeof metaCreator === 'function';
+  const typeString = type.toString();
+
+  const actionCreator = (...args) => {
+    const payload = finalPayloadCreator(...args);
     const action = {
       type,
     };
 
-    const payload = finalActionCreator(...args);
-    if (!(payload === null || payload === undefined)) {
-      action.payload = payload;
-    }
-
-    if (action.payload instanceof Error) {
-      // Handle FSA errors where the payload is an Error object. Set error.
+    if (payload instanceof Error) {
       action.error = true;
     }
 
-    if (typeof metaCreator === 'function') {
+    if (payload !== undefined) {
+      action.payload = payload;
+    }
+
+    if (hasMeta) {
       action.meta = metaCreator(...args);
     }
 
     return action;
   };
 
-  actionHandler.toString = () => type;
+  actionCreator.toString = () => typeString;
 
-  return actionHandler;
+  return actionCreator;
 };
--- app/lib/createReducer.js
+++ app/lib/createReducer.js
@@ -2,9 +2,9 @@ import 'babel-polyfill';
 
 /**
  * Reducer Factory
- * @param {*} initialState 默认值
- * @param {Object} handlers 处理函数映射表
- * @return {Function} Reducer
+ * @param {*} initialState - state tree特定部分的默认值
+ * @param {Object} handlers - 处理函数映射表
+ * @return {function} Reducer
  */
 export default (initialState, handlers) => (state = initialState, action) => {
   if ({}.hasOwnProperty.call(handlers, action.type)) {
