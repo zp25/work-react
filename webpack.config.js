89f9ec47519e7ed21caa56b8ec0ef1065f347035
zp25 <zebrap25@gmail.com>
2017-09-26 15:51:11 +0800

    build: development环境显示更少信息

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,8 @@ module.exports = (env) => {
     historyApiFallback: true,
     stats: {
       colors: true,
-      maxModules: 15,
       chunks: false,
+      modules: false,
       hash: true,
       timings: true,
       version: true,
