26eadbde9df7937005459a0b2c059d724849a19f
zp25 <zebrap25@gmail.com>
2018-04-17 09:32:27 +0800

    fix: 参数错误

--- webpackConfig/index.js
+++ webpackConfig/index.js
@@ -2,12 +2,8 @@ const rules = require('./rules');
 const plugins = require('./plugins');
 const optimization = require('./optimization');
 
-if (process.env.NODE_ENV === 'production') {
-  exports.rules = rules({ dev: false });
-  exports.plugins = plugins({ dev: false });
-  exports.optimization = optimization({ dev: false });
-} else {
-  exports.rules = rules({ dev: true });
-  exports.plugins = plugins({ dev: true });
-  exports.optimization = optimization({ dev: false });
-}
+const dev = process.env.NODE_ENV !== 'production';
+
+exports.rules = rules({ dev });
+exports.plugins = plugins({ dev });
+exports.optimization = optimization({ dev });
