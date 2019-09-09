0f12dafa3a5d9428e1bb47e8989805198d1aa4bf
zp25 <zebrap25@gmail.com>
2019-09-09 13:43:03 +0800

    storybook配置全局decorator和样式

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+
+import '../node_modules/normalize.css/normalize.css';
+import style from '../app/pages/app.scss';
+
+const AppDecorator = storyFn => <div className={style.app}>{storyFn()}</div>;
 
 function loadStories() {
   const req = require.context('../stories', true, /\.stories\.jsx?$/);
@@ -6,3 +12,4 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+addDecorator(AppDecorator);
--- stories/spinner.stories.jsx
+++ stories/spinner.stories.jsx
@@ -3,9 +3,6 @@ import { storiesOf } from '@storybook/react';
 
 import Spinner from '../app/components/spinner';
 
-import '../node_modules/normalize.css/normalize.css';
-import '../app/pages/app.scss';
-
 storiesOf('Spinner', module)
   .add('default', () => (
     <Spinner />
